96516dab889b0b6910d4d7156d5d29ecdd71ec15
Michael Pellegrini <pellegm@amazon.com>
2014-07-13 22:08:38 -0700

    Created handlers directory

--- _includes/schedule-template.php
+++ _includes/schedule-template.php
@@ -11,46 +11,48 @@
             </h4>
         </div>
 
-        <div id="panel-{{id}}" class="panel-collapse collapse">
-            <h6 class="form-instructions">Pick games by clicking team icons, then click the submit button at the bottom of the form.</h6>
-            <div class="panel-body">
-                <form id="{{id}}-form" name="{{id}}-form" method="POST" action="home.php">
-                {{#each games}}
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="team-icons-container">
-                                <span id="{{id}}-gametime" data-gametime="{{gametime}}" class="bottom-right gametime"></span>
-                                <label class="away-team-container team-container">
-                                    <img src="img/{{away}}.png" alt="{{away}}" id="{{id}}-away" class="pick-logo" />
-                                    <input type="radio" id="{{id}}" name="{{id}}" value="{{away}}" />
-                                    <p class="team-name">{{away}}</p>
-                                </label>
-                                <span class="at">@</span>
-                                <label class="home-team-container team-container">
-                                    <img src="img/{{home}}.png" alt="{{home}}" id="{{id}}-home" class="pick-logo" />
-                                    <input type="radio" id="{{id}}" name="{{id}}" value="{{home}}" />
-                                    <p class="team-name">{{home}}</p>
-                                </label>
+        <form method="POST" action="handlers/{{id}}.php" name="{{id}}" id="{{id}}">
+            <div id="panel-{{id}}" class="panel-collapse collapse">
+                <h6 class="form-instructions">Pick games by clicking team icons, then click the submit button at the bottom of the form.</h6>
+                <div class="panel-body">
+                    <form id="{{id}}-form" name="{{id}}-form" method="POST" action="home.php">
+                    {{#each games}}
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="team-icons-container">
+                                    <span id="{{id}}-gametime" data-gametime="{{gametime}}" class="bottom-right gametime"></span>
+                                    <label class="away-team-container team-container">
+                                        <img src="img/{{away}}.png" alt="{{away}}" id="{{id}}-away" class="pick-logo" />
+                                        <input type="radio" id="{{id}}" name="{{id}}" value="{{away}}" />
+                                        <p class="team-name">{{away}}</p>
+                                    </label>
+                                    <span class="at">@</span>
+                                    <label class="home-team-container team-container">
+                                        <img src="img/{{home}}.png" alt="{{home}}" id="{{id}}-home" class="pick-logo" />
+                                        <input type="radio" id="{{id}}" name="{{id}}" value="{{home}}" />
+                                        <p class="team-name">{{home}}</p>
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                {{/each}}
-                    <table class="submit-button-container">
-                        <tr>
-                            <td align="center">
-                                <label for="{{id}}-tiebreaker">Total points on Monday night:</label>
-                                <input type="text" name="{{id}}-tiebreaker" id="{{id}}-tiebreaker" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td align="center">
-                                <input type="submit" name="{{id}}-submit" id="{{id}}-submit" value="Submit {{week}} Picks" class="btn btn-success submit-picks-btn" />
-                            </td>
-                        </tr>
-                    </table>
-                </form>
+                    {{/each}}
+                        <table class="submit-button-container">
+                            <tr>
+                                <td align="center">
+                                    <label for="{{id}}-tiebreaker">Total points on Monday night:</label>
+                                    <input type="text" name="{{id}}-tiebreaker" id="{{id}}-tiebreaker" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center">
+                                    <input type="submit" name="{{id}}-submit" id="{{id}}-submit" value="Submit {{week}} Picks" class="btn btn-success submit-picks-btn" />
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
 {{/each}}
