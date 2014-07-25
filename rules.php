89aae81fb73dc2ce29380ed02b1dbe0aadebebeb
Michael Pellegrini <pellegrinimichael@gmail.com>
2014-07-24 21:13:39 -0700

    Added list of rules

--- rules.php
+++ rules.php
@@ -10,8 +10,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-8 col-md-offset-2">
                 <h1>Rules</h1>
-                <div class="panel panel-default clearfix">
-
+                <div class="panel panel-default login-container-outer clearfix">
+                    <div class="panel panel-default login-container-inner">
+                        <h3>By using this website you agree to the following rules. Failure to comply with these rules may result in the removal of your picks, or the removal of your account.</h3>
+                        <ul class="rules-list">
+                            <li><strong>All picks are final.</strong> Once you submit your picks for a week, those picks are final and cannot be modified.</li>
+                            <li><strong>Picks for each week are due by Thursday at 5:30pm PST.</strong> Picks submitted later than 5:30pm on Thursday will be rejected.</li>
+                            <li><strong>The total points scored on Monday night will be used for a tie&ndash;breaker.</strong> In the event of a tie in number of wins in a week, the player who guesses <strong>CLOSEST</strong> to the actual number of total points scored by both teams combined on Monday night will be determined the winner.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
