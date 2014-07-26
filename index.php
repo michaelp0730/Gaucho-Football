f0a0951ad32052a7c70f72dc383e2e90a42ba011
Michael Pellegrini <pellegrinimichael@gmail.com>
2014-07-25 18:19:28 -0700

    Updating grid system and media queries

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 ?>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-8 col-md-offset-2">
+            <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
             <?php
             if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
             ?>
