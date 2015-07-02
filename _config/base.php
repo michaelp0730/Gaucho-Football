aa8ecf47761deabbfd717dbb0d5b8a06c296a0f4
Michael Pellegrini <mipelleg@microsoft.com>
2015-07-02 13:57:39 -0700

    Connecting to db

--- _config/base.php
+++ _config/base.php
@@ -1,10 +1,15 @@
 <?php
+// ALERT!!!
+// IMPORTANT!!!
+// DO NOT UPLOAD THIS FILE TO YOUR SERVER WITH THESE CONFIG VALUES!!!
+// YOU'LL FUCK EVERYTHING UP!
+// USE THE CORRECT VALUES FROM YOUR HOSTING ACCOUNT!!!
 session_start();
 
 $dbhost = "localhost";
-$dbname = "GauchoFootball2015";
-$dbuser = "root";
-$dbpass = "root";
+$dbname = "my_db_name";
+$dbuser = "my_db_user";
+$dbpass = "my_db_password";
 
 mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
