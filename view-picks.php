fd2af32cc8b73bfaa07d9bac616189bdc9dbf1bd
Michael Pellegrini <pellegrinimichael@gmail.com>
2015-07-03 09:02:12 -0700

    Finished fixing bugs on view picks page

--- view-picks.php
+++ view-picks.php
@@ -601,7 +601,7 @@
                                                 <td class="<?php if ($wk4row[11] == '') { echo 'success'; $wk4_total_wins++; } ?>"><?php echo $wk4row[11]; ?> </td>
                                                 <td class="<?php if ($wk4row[12] == '') { echo 'success'; $wk4_total_wins++; } ?>"><?php echo $wk4row[12]; ?> </td>
                                                 <td class="<?php if ($wk4row[13] == '') { echo 'success'; $wk4_total_wins++; } ?>"><?php echo $wk4row[13]; ?> </td>
-                                                <td><?php $wk4row[14]; ?></td>
+                                                <td><?php echo $wk4row[14]; ?></td>
                                                 <td><?php echo $wk4_total_wins; ?></td>
                                             </tr>
                                         <?php } ?>
@@ -740,7 +740,7 @@
                                                 <td class="<?php if ($wk5row[11] == '') { echo 'success'; $wk5_total_wins++; } ?>"><?php echo $wk5row[11]; ?> </td>
                                                 <td class="<?php if ($wk5row[12] == '') { echo 'success'; $wk5_total_wins++; } ?>"><?php echo $wk5row[12]; ?> </td>
                                                 <td class="<?php if ($wk5row[13] == '') { echo 'success'; $wk5_total_wins++; } ?>"><?php echo $wk5row[13]; ?> </td>
-                                                <td><?php $wk5row[14]; ?></td>
+                                                <td><?php echo $wk5row[14]; ?></td>
                                                 <td><?php echo $wk5_total_wins; ?></td>
                                             </tr>
                                         <?php } ?>
@@ -873,7 +873,7 @@
                                                 <td class="<?php if ($wk6row[5] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[5]; ?> </td>
                                                 <td class="<?php if ($wk6row[6] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[6]; ?> </td>
                                                 <td class="<?php if ($wk6row[7] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[7]; ?> </td>
-                                                <td class="warning <?php if ($wk6row[8] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[8]; ?> </td>
+                                                <td class="<?php if ($wk6row[8] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[8]; ?> </td>
                                                 <td class="<?php if ($wk6row[9] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[9]; ?> </td>
                                                 <td class="<?php if ($wk6row[10] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[10]; ?> </td>
                                                 <td class="<?php if ($wk6row[11] == '') { echo 'success'; $wk6_total_wins++; } ?>"><?php echo $wk6row[11]; ?> </td>
